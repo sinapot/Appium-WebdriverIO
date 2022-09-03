42246e3a3c30bbcd140d50b711ec7ba8124baa7b
sinapot <omai_edison@yahoo.com>
2022-09-03 12:04:51 +1200

    Find elements by UIAutomator and multiple elements

--- test/pageobjects/dialog.page.js
+++ test/pageobjects/dialog.page.js
@@ -1,9 +1,10 @@
 class Dialog{
 
-    //using Accessibility ID
+    //finding elemetns by Accessibility ID
     get appBtn () {return $('~App')};
     get alertDialogsBtn () {return $('~Alert Dialogs')}
     get textEntryBtn () {return $('~Text Entry dialog')}
+    //finding elemetns by xPath
     get usernameText () {return $('//android.widget.EditText[@resource-id="io.appium.android.apis:id/username_edit"]')};
     get password () { return $('//android.widget.EditText[@resource-id="io.appium.android.apis:id/password_edit"]')}
 
