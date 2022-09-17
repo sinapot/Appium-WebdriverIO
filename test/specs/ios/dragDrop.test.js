9ee5e96530ba6e71230a85c2bccf84e4a5374eb8
edisonmaningat <omai_edison@yahoo.com>
2022-09-17 12:15:32 +1200

    all IOS test working before adding new suite

---
+++ test/specs/ios/dragDrop.test.js
@@ -0,0 +1,20 @@
+//using iOS XCUITEST Mobile Gestures
+
+describe("Drag and Drop using WebdriverIO app", async()=>{
+    it("drag and drop the puzzle", async ()=>{
+        
+        await (await $('~Sliders')).click();
+        await (await $('(//XCUIElementTypeSlider)[1]')).addValue(0);
+        await (await $('(//XCUIElementTypeSlider)[1]')).addValue(1);
+
+       const value = await (await $('(//XCUIElementTypeSlider)[1]'));
+       const finalValue = await value.getAttribute("value")
+
+       console.log("final value is"+finalValue);
+
+       await expect(finalValue).toEqual("100%");
+
+    })
+
+})
+
