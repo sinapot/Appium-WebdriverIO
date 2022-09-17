137017f3a9de1c664d747c709719e354adb64a8e
edisonmaningat <omai_edison@yahoo.com>
2022-09-17 13:24:03 +1200

    working wdio drag and drop test case

---
+++ test/specs/ios-UIKitCatalog/dialogAlertBox.test.js
@@ -0,0 +1,31 @@
+//iOS Mobile Gesture
+
+describe("dealing with Alerts",async()=>{
+
+    it("accept Alert",async()=>{
+        await $('~Alert Views').click();
+        await $('~Simple').click();
+
+        //using iOS class chain
+        const text = '**/XCUIElementTypeStaticText[`label == "A Short Title Is Best"`]'
+        const alert = await $(`-ios class chain:${text}`);
+        await expect(alert).toHaveText('A Short Title Is Best');
+        await (await $('~OK')).click();
+
+        await expect(alert).not.toExist();
+        
+    })
+
+    it("dismiss Alert",async()=>{
+        await $('~Alert Views').click();
+        await $('~Simple').click();
+        const text = '**/XCUIElementTypeStaticText[`label == "A Short Title Is Best"`]'
+        const alert = await $(`-ios class chain:${text}`);
+        await expect(alert).toHaveText('A Short Title Is Best');
+        
+        await driver.acceptAlert();
+
+        await expect(alert).not.toExist();
+        
+    })
+})
